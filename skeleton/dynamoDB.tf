dfc70016389eafd22ad05097e007fbee7766077d
Ye Bai <ybai5@binghamton.edu>
2022-07-18 16:24:40 -0500

    4th try

--- skeleton/dynamoDB.tf
+++ skeleton/dynamoDB.tf
@@ -1,24 +1,37 @@
 {% if values.enableDynamoDB %}
-resource "aws_dynamodb_table" "basic-dynamodb-table"{
 
+resource "aws_dynamodb_table" "basic-dynamodb-table"{
   name        = "my-dynamo-table"
   hash_key    = "id"
+  range_key      = "GameTitle"
 
-  attributes = [
-    {
-      name = "id"
-      type = "N"
-    },
-    {
-      name = "title"
-      type = "S"
-    },
-    {
-      name = "age"
-      type = "N"
-    }
-  ]
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
 
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
 }
 
 {% endif %}
+
